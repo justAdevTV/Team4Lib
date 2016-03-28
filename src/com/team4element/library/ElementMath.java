fbcbb90c558caca2aeb6cea52fb46ae97a5f1528
Justin Punzalan <justpunz@live.com>
2016-03-27 17:04:06 -0700

    Made ElementMath better

--- src/com/team4element/library/ElementMath.java
+++ src/com/team4element/library/ElementMath.java
@@ -2,6 +2,7 @@ package com.team4element.library;
 
 public class ElementMath {
 
+	// Squares the number while keeping it's polarity
 	public static double parabolizeNumber(double number){
 		return number * Math.abs(number);
 	}
